ae7e38a1957b48281bac7711cafa47708bb83072
psyglyphik <psyglyphik@gmail.com>
2015-09-12 10:25:40 -0500

    Changes to be committed: 	modified:   app/views/admin_settings/index.html.erb 	modified:   app/views/admin_settings/show.html.erb 	new file:   config/initializers/gravatar_img_tag.rb

---
+++ config/initializers/gravatar_img_tag.rb
@@ -0,0 +1,4 @@
+GravatarImageTag.configure do |config|
+  config.default_image           = :retro   # Set this to use your own default gravatar image rather then serving up Gravatar's default image [ 'http://example.com/images/default_gravitar.jpg', :identicon, :monsterid, :wavatar, 404 ].
+ 
+end
